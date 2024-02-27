34cd174f3145b42324a05c79983864341e52145c
Webgoonie <webgoonie@gmail.com>
2024-02-27 17:35:04 -0500

    Left Side Bar With Profile as Home Page laid out

---
+++ Client-react/src/components/ProfileSide/ProfileSide.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import LogoSearch from '../LogoSearch'
+import ProfileCard from '../ProfileCard'
+import './ProfileSide.css'
+
+const ProfileSide = () => {
+    return (
+        <div className="ProfileSide">
+            
+
+            <LogoSearch />
+
+            <ProfileCard />
+
+        </div>
+    )
+}
+
+export default ProfileSide
---
+++ Client-react/src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+import ProfileSide from '../../components/ProfileSide'
+
+const Home = () => {
+    return (
+        <div className="Home">
+            <ProfileSide />
+            <div className="postSide">Posts</div>
+            <div className="RightSide">RightSide</div>
+        </div>
+    )
+}
+
+export default Home
